0480ec973b8ed2cb0e9e0018e1bd3ab159466130
Fan Zeng <fanzengau@gmail.com>
2024-07-16 00:05:18 +1000

    Add command line flag to accept input file

---
+++ server/novelgo/cmd/cyclic/README.md
@@ -0,0 +1,6 @@
+To use a file as input:
+go run main.go -input_file 1.txt
+
+To run interactively:
+go run main.go
+
--- server/novelgo/cmd/cyclic/main.go
+++ server/novelgo/cmd/cyclic/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"novelgo/internal/pkg/cyclic"
 )
 
 func main() {
 	fmt.Println("cyclic go game")
-	err := run()
-	if err != nil {
-		fmt.Printf("Program exited with error: %v\n", err)
+	input_file := flag.String("input_file", "", "path to input file")
+	flag.Parse()
+	if len(*input_file) > 0 {
+		fmt.Printf("Using input file: %s\n", *input_file)
+	} else {
+		err := run()
+		if err != nil {
+			fmt.Printf("Program exited with error: %v\n", err)
+		}
 	}
 }
 
