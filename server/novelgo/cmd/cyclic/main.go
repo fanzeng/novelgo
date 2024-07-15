a317b3c31ac29aa9e1bfe0e682323f2137bf6cac
Fan Zeng <fanzengau@gmail.com>
2024-07-15 21:12:35 +1000

    Add scan to take user input

--- server/novelgo/cmd/cyclic/main.go
+++ server/novelgo/cmd/cyclic/main.go
@@ -12,6 +12,10 @@ func main() {
 
 func run() {
 	b := cyclic.NewBoard(5, 5)
+	fmt.Print("Enter row and col coordinates, separated by space: ")
+	var r, c int
+	fmt.Scan(&r, &c)
+	fmt.Printf("row = %d, col = %d\n", r, c)
 	fmt.Printf("board:\n")
 	b.Print()
 }
