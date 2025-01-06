1138fb311396ed4db6e956e13c174a1cc2810166
Fan Zeng <fanzengau@gmail.com>
2025-01-07 00:15:13 +1000

    Support dynamic grid sizes up to width of 12

--- client/novelgo-app/src/components/GridPoint.vue
+++ client/novelgo-app/src/components/GridPoint.vue
@@ -9,9 +9,14 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="font-mono text-center text-4xl text-gray-400">
+  <div class="font-mono text-center text-gray-400 font-thin">
     <template v-if="state <= 1">+</template>
     <template v-else-if="state === 2">●</template>
     <template v-else-if="state === 3">○</template>
   </div>
 </template>
+<style scoped>
+  .font-thin {
+    font-weight: 100;
+  }
+</style>
