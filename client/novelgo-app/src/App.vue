ca929ce35378c8b80aa625f00a2fe49833ee11e2
Fan Zeng <fanzengau@gmail.com>
2025-01-06 18:10:35 +1000

    Add tailwind css

--- client/novelgo-app/src/App.vue
+++ client/novelgo-app/src/App.vue
@@ -1,30 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import GridPoint from '@/components/GridPoint.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<div class="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
+  <h1 class="text-2xl">NovelGo</h1>
+  <p>
+    Novel Go Games
+  </p>
+  <div id="board">
+    <div>
+      <GridPoint /><GridPoint /><GridPoint />
+    </div>
+    <div>
+      <GridPoint /><GridPoint /><GridPoint />
+    </div>
+    <div>
+      <GridPoint /><GridPoint /><GridPoint />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ client/novelgo-app/src/components/GridPoint.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { defineProps } from 'vue';
+</script>
+
+<template>
+  <span>+</span>
+</template>
