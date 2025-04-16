66d65e55d031ace1e5264e10b48fb295a345bf85
Fan Zeng <fanzengau@gmail.com>
2025-04-17 00:39:27 +1000

    Revert "Revert cors changes"

--- server/novelgo/internal/pkg/restapi/configure_novelgo.go
+++ server/novelgo/internal/pkg/restapi/configure_novelgo.go
@@ -9,7 +9,6 @@ import (
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
-	"github.com/rs/cors"
 
 	"novelgo/internal/pkg/handlers"
 	"novelgo/internal/pkg/models"
@@ -92,13 +91,14 @@ func configureAPI(api *operations.NovelgoAPI) http.Handler {
 
 	api.ServerShutdown = func() {}
 
-	corsMiddleware := cors.New(cors.Options{
-        AllowedOrigins:   []string{"*"},
-        AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowedHeaders:   []string{"Content-Type"},
-        AllowCredentials: true,
-    })
-    return setupGlobalMiddleware(corsMiddleware.Handler(api.Serve(setupMiddlewares)))
+	// corsMiddleware := cors.New(cors.Options{
+ //        AllowedOrigins:   []string{"*"},
+ //        AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+ //        AllowedHeaders:   []string{"Content-Type"},
+ //        AllowCredentials: true,
+ //    })
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+    // return setupGlobalMiddleware(corsMiddleware.Handler(api.Serve(setupMiddlewares)))
 }
 
 // The TLS configuration before HTTPS server starts.
